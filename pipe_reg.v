dfa8d8647b80d0de07e464e59252715552ca608d
qzrjytl <1254858995@qq.com>
2026-06-23 19:31:37 +0800

    Update pipe_reg.v

--- pipe_reg.v
+++ pipe_reg.v
@@ -9,21 +9,16 @@ module pipe_reg #(
     output reg [WIDTH-1:0] dout
 );
 
-// Pipeline register logic
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-        // Reset: clear output when reset is active
         dout <= 'd0;
     end
     else if (flush) begin
-        // Flush: clear the pipeline register
         dout <= 'd0;
     end
     else if (!stall) begin
-        // Normal operation: transfer input to output on clock edge
         dout <= din;
     end
-    // else: stall is active, hold current value (no change)
 end
 
 endmodule
